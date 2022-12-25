e19df47e3a0a238637293c9de2801ac904c2d142
Songonha <105191398+songonha@users.noreply.github.com>
2022-12-25 09:13:10 +0700

    Update sample_code.js

--- example_ssl/sample_code.js
+++ example_ssl/sample_code.js
@@ -1,28 +1,18 @@
 ### Sample 1:
-
 const WebSocket = require("ws").Server;
-
 const HttpsServer = require('https').createServer;
-
 const fs = require("fs");
 
-
 server = HttpsServer({
-
   cert: fs.readFileSync(config.ssl_cert_path),
-  
   key: fs.readFileSync(config.ssl_key_path)
-
 })
 
 socket = new WebSocket({
-
   server: server
-
 });
 
-
 socket.on(...);
-
 server.listen(config.port);
-
+============================================================================
+### Sample 1:
